b4f2b65926fd3a0f3d568141ce1f7357912b5013
mcbassi <70554606+mcbassi@users.noreply.github.com>
2025-09-14 22:02:21 -0300

    Ajustes de login (backend Consumidor, CSRF, URLs)

--- portal_de_conteudo_web/urls.py
+++ portal_de_conteudo_web/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web_page_app.urls')),  # Inclua as URLs da sua aplicação
+    
 ]
 
 if settings.DEBUG:
